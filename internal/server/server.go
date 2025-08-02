5b896c89c50e073ad31830a41c8ac920e9de1c5b
Terran Stone <terran.stone@maine.edu>
2025-08-01 23:01:21 -0400

    added client page handler

--- internal/server/server.go
+++ internal/server/server.go
@@ -9,18 +9,28 @@ import (
 
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
-	homepage := r.URL.Path
-	if homepage == "/" {
+	if r.URL.Path == "/" {
 		// Serve the index.html file from the templates directory
-		filepath := filepath.Join("templates", "index.html")
-		http.ServeFile(w, r, filepath)
+		path := filepath.Join("templates", "index.html")
+		http.ServeFile(w, r, path)
 		return
 	}
 	// Serve 404 for any other path
 	http.NotFound(w, r)
 }
 
+func clientHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/clients" {
+		// Server the client.html file from the templates directory
+		path := filepath.Join("templates", "clients.html")
+		http.ServeFile(w, r, path)
+	}
+	// Server 404 for any other path
+	http.NotFound(w, r)
+}
+
 func Start() {
+	http.HandleFunc("/clients", clientHandler)
 	http.HandleFunc("/", homeHandler)
 	log.Println("Server started on :8080")
 	log.Println("Press Ctrl+C to stop the server.")
