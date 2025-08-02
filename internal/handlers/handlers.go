d90979553d4868236675066ca9b2becac51cc68a
Terran Stone <terran.stone@maine.edu>
2025-08-01 23:19:24 -0400

    Separated handlers from server

---
+++ internal/handlers/handlers.go
@@ -0,0 +1,37 @@
+package handlers
+
+import (
+	"net/http"
+	"path/filepath"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		// Serve the index.html file from the templates directory
+		path := filepath.Join("templates", "index.html")
+		http.ServeFile(w, r, path)
+		return
+	}
+	// Serve 404 for any other path
+	http.NotFound(w, r)
+}
+
+func ClientHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/clients" {
+		// Server the client.html file from the templates directory
+		path := filepath.Join("templates", "clients.html")
+		http.ServeFile(w, r, path)
+	}
+	// Server 404 for any other path
+	http.NotFound(w, r)
+}
+
+func EventHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/events" {
+		// Serve the events.html file from the templates directory
+		path := filepath.Join("templates", "events.html")
+		http.ServeFile(w, r, path)
+		return
+	}
+	http.NotFound(w, r)
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -3,44 +3,15 @@ package server
 import (
 	"log"
 	"net/http"
-	"path/filepath"
+
+	"noreast_cms/internal/handlers"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/" {
-		// Serve the index.html file from the templates directory
-		path := filepath.Join("templates", "index.html")
-		http.ServeFile(w, r, path)
-		return
-	}
-	// Serve 404 for any other path
-	http.NotFound(w, r)
-}
-
-func clientHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/clients" {
-		// Server the client.html file from the templates directory
-		path := filepath.Join("templates", "clients.html")
-		http.ServeFile(w, r, path)
-	}
-	// Server 404 for any other path
-	http.NotFound(w, r)
-}
-
-func eventHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/events" {
-		// Serve the events.html file from the templates directory
-		path := filepath.Join("templates", "events.html")
-		http.ServeFile(w, r, path)
-		return
-	}
-	http.NotFound(w, r)
-}
 
 func Start() {
-	http.HandleFunc("/events", eventHandler)
-	http.HandleFunc("/clients", clientHandler)
-	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/events", handlers.EventHandler)
+	http.HandleFunc("/clients", handlers.ClientHandler)
+	http.HandleFunc("/", handlers.HomeHandler)
 	log.Println("Server started on :8080")
 	log.Println("Press Ctrl+C to stop the server.")
 	log.Fatal(http.ListenAndServe(":8080", nil))
